820ba21a24c6256756c3a4f273848f9819be4954
Liliana Barrera <barrera.jimnlil@gmail.com>
2023-01-04 14:52:14 -0500

    Completed and Passed up to and including Exercise 17

--- exercises/e17.js
+++ exercises/e17.js
@@ -1,3 +1,17 @@
-export function minBy(array, cb) {}
+export function minBy(array, cb) {
+	let personAge = array[0];
+	for (let element of array) {
+		if (cb(element) < personAge.age) {
+		  return element;
+    }
+	}	
+}
 
-export function maxBy(array, cb) {}
+export function maxBy(array, cb) {
+	let personAge = array[0];
+	for (let element of array) {
+		if (cb(element) > cb(personAge)){
+		  return element;
+    }
+	}	
+}

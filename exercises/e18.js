e77a8d3e9255669919c375805fe336564c855547
Liliana Barrera <barrera.jimnlil@gmail.com>
2023-01-04 17:09:04 -0500

    Finished-Completed all exercises 1-18

--- exercises/e18.js
+++ exercises/e18.js
@@ -4,10 +4,23 @@ import { data } from "../data/data";
 // Return the year with the greatest number of Asteroids discoveries
 // Return example: 1902
 
-export function getGreatestDiscoveryYear(data) {
+export function getGreatestDiscoveryYear({asteroids}) {
   // Your code goes here...
   // feel free to import your `maxBy` or `minBy` methods from previous lessons
-}
+  const listOfYears = asteroids.map((asteroid) => asteroid.discoveryYear);
+   
+  const tallies = listOfYears.reduce((acc, val) => {
+    acc[val] = (acc[val] || 0) + 1;
+    return acc;
+  }, {})
+   
+  const results = Object.keys(tallies).filter(x => {
+    return tallies[x] == Math.max.apply(null, Object.values(tallies))
+  })[0];
+
+  return parseInt(results);
+  }
+
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-16"
